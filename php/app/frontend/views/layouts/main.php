bb26b62314d3243fe5aa2039e62d417612b0bc7b
Caravus <caravus@caravus.org>
2017-03-15 11:53:53 +0300

    ui

--- php/app/frontend/views/layouts/main.php
+++ php/app/frontend/views/layouts/main.php
@@ -5,79 +5,148 @@
  */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use frontend\assets\AppAsset;
-use common\widgets\Alert;
+use app\assets\AltairAsset;
 
-AppAsset::register($this);
+AltairAsset::register($this);
 $this->beginPage();
 ?>
-    <!DOCTYPE html>
-    <html lang="<?= Yii::$app->language ?>">
+    <!doctype html>
+    <!--[if lte IE 9]>
+    <html class="lte-ie9" lang="en">
+    <![endif]-->
+    <!--[if gt IE 9]><!-->
+    <html lang="en">
+    <!--<![endif]-->
     <head>
-        <meta charset="<?= Yii::$app->charset ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Remove Tap Highlight on Windows Phone IE -->
+        <meta name="msapplication-tap-highlight" content="no"/>
+
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php
         $this->head();
         ?>
     </head>
-    <body>
+    <body class=" sidebar_main_open sidebar_main_swipe">
     <?php
     $this->beginBody();
     ?>
-    <div class="wrap">
-        <?php
-        NavBar::begin([
-            'brandLabel' => 'My Company',
-            'brandUrl' => Yii::$app->homeUrl,
-            'options' => [
-                'class' => 'navbar-inverse navbar-fixed-top',
-            ],
-        ]);
+    <!-- main header -->
+    <header id="header_main">
+        <div class="header_main_content">
+            <nav class="uk-navbar">
 
-        $menuItems = [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'LP', 'url' => ['/landing-page/designer']],
-        ];
+                <!-- main sidebar switch -->
+                <a href="#" id="sidebar_main_toggle" class="sSwitch sSwitch_left">
+                    <span class="sSwitchIcon"></span>
+                </a>
 
-        /*
-        if (Yii::$app->user->isGuest) {
-            $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-            $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-        } else {
-            $menuItems[] = '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>';
-        }
-        */
+                <!-- secondary sidebar switch -->
+                <!--
+                                <a href="#" id="sidebar_secondary_toggle" class="sSwitch sSwitch_right sidebar_secondary_check">
+                                    <span class="sSwitchIcon"></span>
+                                </a>
+                -->
+                <!-- TOP MENU
+                                    <div id="menu_top_dropdown" class="uk-float-left uk-hidden-small">
+                                        <div class="uk-button-dropdown" data-uk-dropdown="{mode:'click'}">
+                                            <a href="#" class="top_menu_toggle"><i class="material-icons md-24">&#xE8F0;</i></a>
+                                            <div class="uk-dropdown uk-dropdown-width-3">
+                                                <div class="uk-grid uk-dropdown-grid" data-uk-grid-margin>
+                                                    <div class="uk-width-2-3">
+                                                        <div class="uk-grid uk-grid-width-medium-1-3 uk-margin-top uk-margin-bottom uk-text-center" data-uk-grid-margin>
+                                                            <a href="page_mailbox.html">
+                                                                <i class="material-icons md-36">&#xE158;</i>
+                                                                <span class="uk-text-muted uk-display-block">Mailbox</span>
+                                                            </a>
+                                                            <a href="page_invoices.html">
+                                                                <i class="material-icons md-36">&#xE53E;</i>
+                                                                <span class="uk-text-muted uk-display-block">Invoices</span>
+                                                            </a>
+                                                            <a href="page_chat.html">
+                                                                <i class="material-icons md-36 md-color-red-600">&#xE0B9;</i>
+                                                                <span class="uk-text-muted uk-display-block">Chat</span>
+                                                            </a>
+                                                            <a href="page_scrum_board.html">
+                                                                <i class="material-icons md-36">&#xE85C;</i>
+                                                                <span class="uk-text-muted uk-display-block">Scrum Board</span>
+                                                            </a>
+                                                            <a href="page_snippets.html">
+                                                                <i class="material-icons md-36">&#xE86F;</i>
+                                                                <span class="uk-text-muted uk-display-block">Snippets</span>
+                                                            </a>
+                                                            <a href="page_user_profile.html">
+                                                                <i class="material-icons md-36">&#xE87C;</i>
+                                                                <span class="uk-text-muted uk-display-block">User profile</span>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="uk-width-1-3">
+                                                        <ul class="uk-nav uk-nav-dropdown uk-panel">
+                                                            <li class="uk-nav-header">Components</li>
+                                                            <li><a href="components_accordion.html">Accordions</a></li>
+                                                            <li><a href="components_buttons.html">Buttons</a></li>
+                                                            <li><a href="components_notifications.html">Notifications</a></li>
+                                                            <li><a href="components_sortable.html">Sortable</a></li>
+                                                            <li><a href="components_tabs.html">Tabs</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                -->
+                <div class="uk-navbar-flip">
+                    <ul class="uk-navbar-nav user_actions">
+                        <li>
+                            <a href="#" id="full_screen_toggle" class="user_action_icon uk-visible-large">
+                                <i class="material-icons md-24 md-light">&#xE5D0;</i>
+                            </a>
+                        </li>
 
-        echo Nav::widget([
-            'options' => ['class' => 'navbar-nav navbar-right'],
-            'items' => $menuItems,
-        ]);
-        NavBar::end();
-        ?>
-        <br/>
-        <br/>
-        <br/>
-        <div class="container">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-            <?= Alert::widget() ?>
-            <?= $content ?>
+                        <!--
+                        <li data-uk-dropdown="{mode:'click',pos:'bottom-right'}">
+                            <a href="#" class="user_action_image">
+                                <img class="md-user-image" src="altair/assets/img/user.png" alt=""/>
+                            </a>
+
+                            <div class="uk-dropdown uk-dropdown-small">
+                                <ul class="uk-nav js-uk-prevent">
+                                    <li><a href="/">My profile</a></li>
+                                    <li><a href="/">Settings</a></li>
+                                    <li><a href="/">Logout</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        -->
+                    </ul>
+                </div>
+            </nav>
         </div>
-    </div>
+        <div class="header_main_search_form">
+            <i class="md-icon header_main_search_close material-icons">&#xE5CD;</i>
+            <form class="uk-form">
+                <input type="text" class="header_main_search_input"/>
+                <button class="header_main_search_btn uk-button-link"><i class="md-icon material-icons">&#xE8B6;</i>
+                </button>
+            </form>
+        </div>
+    </header><!-- main header end -->
 
+    <!-- main sidebar -->
+    <?= $this->render('sidebar') ?>
+    <!-- main sidebar end -->
+
+    <aside>
+        <div id="page_content">
+            <div id="page_content_inner">
+                <?= $content ?>
+            </div>
+        </div>
+    </aside><!-- secondary sidebar end -->
     <?php
     $this->endBody();
     ?>
---
+++ php/app/frontend/views/layouts/sidebar.php
@@ -0,0 +1,42 @@
+<?php
+use yii\helpers\Url;
+
+?>
+<aside id="sidebar_main">
+    <div style="text-align: center">
+        <a href="/"><img src="/altair/assets/img/LinkIT360_logo.png"/></a>
+    </div>
+
+    <div class="menu_section">
+        <ul>
+            <li class="current_section" title="Dashboard">
+                <a href="<?= Url::to('/site/index') ?>">
+                    <span class="menu_icon"><i class="material-icons">&#xE871;</i></span>
+                    <span class="menu_title">DASHBOARD</span>
+                </a>
+            </li>
+
+            <li title="Reports">
+                <a>
+                    <span class="menu_icon"><i class="material-icons">&#xE85C;</i></span>
+                    <span class="menu_title">REPORTS</span>
+                </a>
+                <ul>
+                    <!--<li><a href="/reports">Revenue</a></li>-->
+                    <!--<li><a href="/conversion-report">Conversion</a></li>-->
+                    <li><a href="<?= Url::to('/ad-report/index') ?>">Advertising</a></li>
+                </ul>
+            </li>
+
+            <li title="Tools">
+                <a href="#">
+                    <span class="menu_icon"><i class="material-icons">&#xE8B8;</i></span>
+                    <span class="menu_title">TOOLS</span>
+                </a>
+                <ul>
+                    <li><a href="<?= Url::to('/landing-page/designer') ?>">Landing Page Designer</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</aside>

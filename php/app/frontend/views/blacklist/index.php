4d521076c56953302ce1e24fbdcce80611ed0982
Caravus <caravus@caravus.org>
2017-03-20 12:09:28 +0300

    Blacklist CRUD

--- php/app/frontend/views/blacklist/index.php
+++ php/app/frontend/views/blacklist/index.php
@@ -2,25 +2,42 @@
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\web\View;
 use yii\widgets\ActiveForm;
 
 /**
- * @var yii\web\View                   $this
+ * @var View                           $this
  * @var yii\data\ActiveDataProvider    $dataProvider
  * @var \common\models\MsisdnBlacklist $model
  */
 
 $this->title = 'MSISDN Blacklist';
 $this->params['breadcrumbs'][] = $this->title;
+
+/** @noinspection JSUnusedGlobalSymbols */
+$js = <<<JS
+    function check_msisdn(obj) {
+        var input = $(obj);
+        input.val(input.val().replace(/\D/g, ''));
+    }
+JS;
+
+$this->registerJs($js, View::POS_BEGIN)
 ?>
+<div class="small-header transition animated fadeIn">
+    <div class="hpanel">
+        <div class="panel-body">
+            <h2 class="font-light m-b-xs">
+                <?= Html::encode($this->title) ?>
+            </h2>
+        </div>
+    </div>
+</div>
+
 <div class="content animate-panel">
     <div class="row">
-        <div class="hpanel">
+        <div class="hpanel col-lg-6">
             <div class="panel-body">
-                <h1>
-                    <?= Html::encode($this->title) ?>
-                </h1>
-
                 <p>
                     <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">
                         Add MSISDN
@@ -34,36 +51,54 @@ $this->params['breadcrumbs'][] = $this->title;
                         'msisdn',
                         [
                             'header' => 'Country',
+                            'headerOptions' => [
+                                'width' => '140',
+                                'class' => 'text-right'
+                            ],
                             'contentOptions' => function () {
                                 return ['class' => 'text-right'];
                             },
                             'content' => function ($data) use ($model) {
-                                return $model->countries[$model->providers[$data['id_provider']]['id_country']];
+                                return $model->getCountries()[$model->getProviders()[$data['id_provider']]['id_country']];
                             }
                         ],
                         [
                             'attribute' => 'id_operator',
+                            'headerOptions' => [
+                                'width' => '140',
+                                'class' => 'text-right'
+                            ],
                             'contentOptions' => function () {
                                 return ['class' => 'text-right'];
                             },
                             'content' => function ($data) use ($model) {
-                                return $model->operators[$data['id_operator']];
+                                return $model->getOperators()[$data['id_operator']];
                             }
                         ],
                         [
                             'attribute' => 'id_provider',
+                            'headerOptions' => [
+                                'width' => '140',
+                                'class' => 'text-right'
+                            ],
                             'contentOptions' => function () {
                                 return ['class' => 'text-right'];
                             },
                             'content' => function ($data) use ($model) {
-                                return $model->providers[$data['id_provider']]['name'];
+                                return $model->getProviders()[$data['id_provider']]['name'];
                             }
                         ],
-                        'created_at',
+                        [
+                            'attribute' => 'created_at',
+                            'headerOptions' => ['width' => '140'],
+                            'content' => function ($data) {
+                                return date('Y-m-d H:i:s', strtotime($data->created_at));
+                            }
+                        ],
                         [
                             'class' => 'yii\grid\ActionColumn',
                             'header' => '',
-                            'headerOptions' => ['width' => '80'],
+                            'headerOptions' => ['width' => '50'],
                             'template' => '{view}{delete}',
                         ],
                     ],
@@ -94,10 +129,20 @@ $this->params['breadcrumbs'][] = $this->title;
             ?>
             <div class="modal-body">
                 <?php
-                echo $form->field($model, 'msisdn')->textInput(['maxlength' => true]);
+                echo $form->field($model, 'msisdn')->textInput([
+                    'maxlength' => true,
+                    'onkeyup' => 'check_msisdn(this);'
+                ]);
+
                 echo $form->field($model, 'id_provider')
-                    ->dropDownList(ArrayHelper::map($model->providers, 'id', 'name'));
-                echo $form->field($model, 'id_operator')->dropDownList($model->operators);
+                    ->dropDownList(
+                        ArrayHelper::map($model->getProviders(), 'id', 'name'),
+                        [
+//                            'onchange' => 'change_operators(this);'
+                        ]
+                    );
+
+                echo $form->field($model, 'id_operator')->dropDownList($model->getOperators());
                 ?>
             </div>
 
@@ -111,3 +156,12 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
 </div>
+
+<!--
+<script type="text/javascript">
+    function change_operators(obj) {
+        var provider = $(obj);
+        console.log(provider.val());
+    }
+</script>
+-->

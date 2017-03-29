92c1b00d5f84d441e78ee6da8f424ceca65cbcc4
Caravus <caravus@caravus.org>
2017-03-29 13:58:45 +0300

    golang

--- go/acceptor/src/handlers/handlers.go
+++ go/acceptor/src/handlers/handlers.go
@@ -12,8 +12,7 @@ func init() {
 
 type Response struct{}
 
-type Aggregate struct {
-}
+type Aggregate struct{}
 
 type AggregateData struct {
 	Aggregated []base.Aggregate `json:"aggregated,omitempty"`
@@ -21,4 +20,18 @@ type AggregateData struct {
 
 func (rpc *Aggregate) Receive(req AggregateData, res *Response) error {
 	return base.SaveRows(req.Aggregated)
+}
+
+type BlackList struct{}
+
+type BlackListParams struct {
+	ProviderName string `json:"provider_name,omitempty"`
+}
+
+type BlackListResponse struct {
+	Msisdns []string `json:"msisdns,omitempty"`
+}
+
+func (rpc *BlackList) Get(req BlackListParams, res *BlackListResponse) error {
+	return nil
 }

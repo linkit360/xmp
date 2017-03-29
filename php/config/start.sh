1c79ab69b1da017acce43b6b3717a95d9f1a412c
Caravus <caravus@caravus.org>
2017-03-29 11:52:53 +0300

    github token

--- php/config/start.sh
+++ php/config/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Composer Github Token
-composer -n global config github-oauth.github.com ${PROJECT_GITHUB}
+# Composer Github Token from vostok-deploy
+composer -n global config github-oauth.github.com 79fb3dea4c9cb40f32be9a4a1e09479a283a68ef
 
 # Migrations
 php /app/php/yii migrate/up --interactive=0

bade2b89321d34d0416d0a8a05b2de92f7778f9c
Caravus <caravus@caravus.org>
2017-04-03 17:50:20 +0300

    theme dashboard

--- php/app/frontend/views/reports/conversion.php
+++ php/app/frontend/views/reports/conversion.php
@@ -16,147 +16,158 @@ $this->params['breadcrumbs'][] = [
 ];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="hpanel col-lg-6">
-    <div class="panel-body">
-        <h4>
-            Filter
-        </h4>
+<div class="col-lg-6">
+    <div class="ibox float-e-margins">
+        <div class="ibox-title">
+            <h5>
+                Filter
+            </h5>
+        </div>
 
-        <?php
-        echo $this->render(
-            'filter',
-            [
-                'model' => $model,
-            ]
-        );
-        ?>
+        <div class="ibox-content">
+            <?php
+            echo $this->render(
+                'filter',
+                [
+                    'model' => $model,
+                ]
+            );
+            ?>
+        </div>
     </div>
 </div>
 
-<div class="hpanel col-lg-6">
-    <div class="panel-body">
-        <h4 class="text-center">
-            Total Lp Hits For Period: <?= $model->chart['sum'] ?>
-        </h4>
-        <?php
-        if (array_key_exists('sum', $model->chart) && $model->chart['sum'] > 0) {
-            echo Highcharts::widget([
-                'options' => [
-                    'chart' => [
-                        'height' => 230,
-                        'zoomType' => 'x',
-                        'type' => 'column',
-                    ],
-                    'title' => [
-                        'useHTML' => true,
-                        'text' => '',
-                    ],
-                    'plotOptions' => [
-                        'series' => [
-                            'fillOpacity' => 0.7,
-                            'lineWidth' => 2,
+<div class="col-lg-6">
+    <div class="ibox float-e-margins">
+        <div class="ibox-title">
+            <h5>
+                Total Lp Hits For Period: <?= $model->chart['sum'] ?>
+            </h5>
+        </div>
+
+        <div class="ibox-content">
+            <?php
+            if (array_key_exists('sum', $model->chart) && $model->chart['sum'] > 0) {
+                echo Highcharts::widget([
+                    'options' => [
+                        'chart' => [
+                            'height' => 235,
+                            'zoomType' => 'x',
+                            'type' => 'column',
                         ],
+                        'title' => [
+                            'useHTML' => true,
+                            'text' => '',
+                        ],
+                        'plotOptions' => [
+                            'series' => [
+                                'fillOpacity' => 0.7,
+                                'lineWidth' => 2,
+                            ],
+                        ],
+                        'xAxis' => [
+                            'gridLineColor' => '#BFC8CE',
+                            'gridLineDashStyle' => 'shortdash',
+                            'gridLineWidth' => '1',
+                            'min' => 0,
+                            'tickmarkPlacement' => 'on',
+                            'categories' => $model->chart['days'],
+                        ],
+                        'yAxis' => [
+                            'gridLineColor' => '#BFC8CE',
+                            'gridLineDashStyle' => 'shortdash',
+                            'gridLineWidth' => '1',
+                            'title' => ['text' => 'Lp Hits'],
+                        ],
+                        'credits' => ['enabled' => false],
+                        'series' => $model->chart['series'],
                     ],
-                    'xAxis' => [
-                        'gridLineColor' => '#BFC8CE',
-                        'gridLineDashStyle' => 'shortdash',
-                        'gridLineWidth' => '1',
-                        'min' => 0,
-                        'tickmarkPlacement' => 'on',
-                        'categories' => $model->chart['days'],
+                ]);
+            } else {
+                echo '<div style="text-align: center;">No data.</div>';
+            }
+            ?>
+        </div>
+    </div>
+</div>
+
+<div class="col-lg-12">
+    <div class="ibox float-e-margins">
+        <div class="ibox-content">
+            <?php
+            echo GridView::widget([
+                'dataProvider' => $model->dataConv(),
+                'columns' => [
+                    [
+                        'attribute' => 'report_date',
+                        'content' => function ($data) {
+                            return date('Y-m-d', strtotime($data['report_at_day']));
+                        },
                     ],
-                    'yAxis' => [
-                        'gridLineColor' => '#BFC8CE',
-                        'gridLineDashStyle' => 'shortdash',
-                        'gridLineWidth' => '1',
-                        'title' => ['text' => 'Lp Hits'],
+                    [
+                        'attribute' => 'id_campaign',
+                        'contentOptions' => function () {
+                            return ['class' => 'text-right'];
+                        },
+                        'content' => function ($data) {
+                            return number_format($data['id_campaign']);
+                        },
                     ],
-                    'credits' => ['enabled' => false],
-                    'series' => $model->chart['series'],
+                    [
+                        'attribute' => 'lp_hits',
+                        'contentOptions' => function () {
+                            return ['class' => 'text-right'];
+                        },
+                        'content' => function ($data) {
+                            return number_format($data['lp_hits']);
+                        },
+                    ],
+                    [
+                        'attribute' => 'lp_msisdn_hits',
+                        'contentOptions' => function () {
+                            return ['class' => 'text-right'];
+                        },
+                        'content' => function ($data) {
+                            return number_format($data['lp_msisdn_hits']);
+                        },
+                    ],
+                    [
+                        'attribute' => 'mo',
+                        'contentOptions' => function () {
+                            return ['class' => 'text-right'];
+                        },
+                        'content' => function ($data) {
+                            return number_format($data['mo']);
+                        },
+                    ],
+                    [
+                        'attribute' => 'mo_success',
+                        'contentOptions' => function () {
+                            return ['class' => 'text-right'];
+                        },
+                        'content' => function ($data) {
+                            return number_format($data['mo_success']);
+                        },
+                    ],
+                    [
+                        'label' => 'Conversion Rate',
+                        'contentOptions' => function () {
+                            return ['class' => 'text-right'];
+                        },
+                        'content' => function ($data) {
+                            $conv = 0;
+                            if ($data['lp_hits']) {
+                                $conv = number_format(
+                                    $data['mo'] / $data['lp_hits'] * 100,
+                                    2
+                                );
+                            }
+                            return '<b>' . $conv . '</b>%';
+                        },
+                    ],
                 ],
             ]);
-        } else {
-            echo '<div style="text-align: center;">No data.</div>';
-        }
-        ?>
-    </div>
-</div>
-
-<div class="hpanel col-lg-12">
-    <div class="panel-body">
-        <?php
-        echo GridView::widget([
-            'dataProvider' => $model->dataConv(),
-            'columns' => [
-                [
-                    'attribute' => 'report_date',
-                    'content' => function ($data) {
-                        return date('Y-m-d', strtotime($data['report_at_day']));
-                    },
-                ],
-                [
-                    'attribute' => 'id_campaign',
-                    'contentOptions' => function () {
-                        return ['class' => 'text-right'];
-                    },
-                    'content' => function ($data) {
-                        return number_format($data['id_campaign']);
-                    },
-                ],
-                [
-                    'attribute' => 'lp_hits',
-                    'contentOptions' => function () {
-                        return ['class' => 'text-right'];
-                    },
-                    'content' => function ($data) {
-                        return number_format($data['lp_hits']);
-                    },
-                ],
-                [
-                    'attribute' => 'lp_msisdn_hits',
-                    'contentOptions' => function () {
-                        return ['class' => 'text-right'];
-                    },
-                    'content' => function ($data) {
-                        return number_format($data['lp_msisdn_hits']);
-                    },
-                ],
-                [
-                    'attribute' => 'mo',
-                    'contentOptions' => function () {
-                        return ['class' => 'text-right'];
-                    },
-                    'content' => function ($data) {
-                        return number_format($data['mo']);
-                    },
-                ],
-                [
-                    'attribute' => 'mo_success',
-                    'contentOptions' => function () {
-                        return ['class' => 'text-right'];
-                    },
-                    'content' => function ($data) {
-                        return number_format($data['mo_success']);
-                    },
-                ],
-                [
-                    'label' => 'Conversion Rate',
-                    'contentOptions' => function () {
-                        return ['class' => 'text-right'];
-                    },
-                    'content' => function ($data) {
-                        $conv = 0;
-                        if ($data['lp_hits']) {
-                            $conv = number_format(
-                                $data['mo'] / $data['lp_hits'] * 100,
-                                2
-                            );
-                        }
-                        return '<b>' . $conv . '</b>%';
-                    },
-                ],
-            ],
-        ]);
-        ?>
+            ?>
+        </div>
     </div>
 </div>
--- php/app/frontend/views/site/index.php
+++ php/app/frontend/views/site/index.php
@@ -21,133 +21,119 @@ if (YII_ENV === "dev") {
 
 $this->registerJs('server = "ws://' . $host . ':3000/echo";');
 ?>
-<div class="hpanel col-md-3">
-    <div class="panel-body">
-        <div class="stats-title pull-left">
-            <h4>LP Hits</h4>
-        </div>
+<div class="col-lg-3">
+    <div class="ibox float-e-margins">
+        <div class="ibox-content">
+            <h1 class="no-margins" id="output_lp">
+                0
+            </h1>
 
-        <div class="stats-icon pull-right">
-            <i class="pe-7s-shuffle fa-4x"></i>
-        </div>
-
-        <div class="m-t-xl">
-            <h1 class="text-info" id="output_lp">0</h1>
+            LP Hits
         </div>
     </div>
 </div>
 
-<div class="hpanel col-md-3">
-    <div class="panel-body">
-        <div class="stats-title pull-left">
-            <h4>Total MO</h4>
-        </div>
+<div class="col-lg-3">
+    <div class="ibox float-e-margins">
+        <div class="ibox-content">
+            <h1 class="no-margins" id="output_mo">
+                0
+            </h1>
 
-        <div class="stats-icon pull-right">
-            <i class="pe-7s-shuffle fa-4x"></i>
-        </div>
-
-        <div class="m-t-xl">
-            <h1 class="text-info" id="output_mo">0</h1>
+            Total MO
         </div>
     </div>
 </div>
 
-<div class="hpanel col-md-3">
-    <div class="panel-body">
-        <div class="stats-title pull-left">
-            <h4>Success MO</h4>
-        </div>
+<div class="col-lg-3">
+    <div class="ibox float-e-margins">
+        <div class="ibox-content">
+            <h1 class="no-margins" id="output_mos">
+                0
+            </h1>
 
-        <div class="stats-icon pull-right">
-            <i class="pe-7s-shuffle fa-4x"></i>
-        </div>
-
-        <div class="m-t-xl">
-            <h1 class="text-info" id="output_mos">0</h1>
+            Success MO
         </div>
     </div>
 </div>
 
-<div class="hpanel col-md-3">
-    <div class="panel-body">
-        <div class="stats-title pull-left">
-            <h4>Conversion Rate</h4>
-        </div>
+<div class="col-lg-3">
+    <div class="ibox float-e-margins">
+        <div class="ibox-content">
+            <h1 class="no-margins" id="output_conv">
+                0
+            </h1>
 
-        <div class="stats-icon pull-right">
-            <i class="pe-7s-shuffle fa-4x"></i>
-        </div>
-
-        <div class="m-t-xl">
-            <h1 class="text-info" id="output_conv">0</h1>
+            Conversion Rate
         </div>
     </div>
 </div>
 
-<div class="hpanel col-md-12">
-    <div class="panel-body">
-        <?php
-        $this->registerJsFile(
-            'http://code.highcharts.com/mapdata/custom/world-eckert3-lowres.js',
-            [
-                'depends' => 'miloschuman\highcharts\HighchartsAsset',
-            ]
-        );
+<div class="col-lg-12">
+    <div class="ibox float-e-margins">
+        <div class="ibox-content">
+            <?php
+            $this->registerJsFile(
+                'http://code.highcharts.com/mapdata/custom/world-eckert3-lowres.js',
+                [
+                    'depends' => 'miloschuman\highcharts\HighchartsAsset',
+                ]
+            );
 
-        $countries = \common\models\Countries::find()
-            ->select([
-                'lower(iso) as iso',
-            ])
-            ->asArray()
-            ->all();
+            $countries = \common\models\Countries::find()
+                ->select([
+                    'lower(iso) as iso',
+                ])
+                ->asArray()
+                ->all();
 
-        $data = [];
-        foreach ($countries as $country) {
-            $data[] = ['hc-key' => $country['iso'], 'value' => 0];
-        }
+            $data = [];
+            foreach ($countries as $country) {
+                $data[] = ['hc-key' => $country['iso'], 'value' => 0];
+            }
 
-        echo Highmaps::widget([
-            'id' => 'hmap',
-            'options' => [
-                'title' => [
-                    'text' => 'Incoming Traffic. World area (LP pages shown)',
-                ],
-                'mapNavigation' => [
-                    'enableMouseWheelZoom' => false,
-                    'enabled' => true,
-                    'buttonOptions' => [
-                        'verticalAlign' => 'bottom',
+            echo Highmaps::widget([
+                'id' => 'hmap',
+                'options' => [
+                    'title' => [
+                        'text' => 'Incoming Traffic. World area (LP pages shown)',
                     ],
-                ],
-                'chart' => [
+                    'mapNavigation' => [
+                        'enableMouseWheelZoom' => false,
+                        'enabled' => true,
+                        'buttonOptions' => [
+                            'verticalAlign' => 'bottom',
+                        ],
+                    ],
+                    'chart' => [
 //                    'width' => 100%,
-                    'height' => 600,
-                ],
-                'colorAxis' => [
-                    'min' => 5,
-                ],
-                'series' => [
-                    [
-                        'data' => $data,
-                        'mapData' => new JsExpression('Highcharts.maps["custom/world-eckert3-lowres"]'),
-                        'joinBy' => 'hc-key',
-                        'name' => 'Incoming Traffic (LP pages shown)',
-                        'allowPointSelect' => true,
-                        'cursor' => 'pointer',
-                        'states' => [
-                            'select' => [
-                                'color' => '#a4edba',
+                        'height' => 600,
+                    ],
+                    'colorAxis' => [
+                        'min' => 5,
+                    ],
+                    'series' => [
+                        [
+                            'data' => $data,
+                            'mapData' => new JsExpression('Highcharts.maps["custom/world-eckert3-lowres"]'),
+                            'joinBy' => 'hc-key',
+                            'name' => 'Incoming Traffic (LP pages shown)',
+                            'allowPointSelect' => true,
+                            'cursor' => 'pointer',
+                            'states' => [
+                                'select' => [
+                                    'color' => '#a4edba',
+                                ],
+                            ],
+                            'dataLabels' => [
+                                'enabled' => true,
+                                'format' => '{point.name}',
                             ],
                         ],
-                        'dataLabels' => [
-                            'enabled' => true,
-                            'format' => '{point.name}',
-                        ],
                     ],
                 ],
-            ],
-        ]);
-        ?>
+            ]);
+            ?>
+        </div>
     </div>
 </div>

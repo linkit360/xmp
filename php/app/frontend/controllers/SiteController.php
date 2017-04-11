6a2d8a01d56bc915062945c41c6f83a7a39c81d9
Caravus <caravus@caravus.org>
2017-04-11 17:02:10 +0300

    soap

--- php/app/frontend/controllers/SiteController.php
+++ php/app/frontend/controllers/SiteController.php
@@ -7,19 +7,11 @@ use function array_keys;
 use common\models\Countries;
 use common\models\Operators;
 use common\models\Providers;
-use common\models\Reports;
 use function count;
 use function date;
-use function dump;
 use const false;
-use function json_decode;
 use function json_encode;
 use const JSON_PRETTY_PRINT;
-use function mt_rand;
-use const PHP_EOL;
-use function print_r;
-use stdClass;
-use function time;
 use const true;
 use Yii;
 use yii\db\Query;
@@ -137,6 +129,7 @@ class SiteController extends Controller
         return $this->goHome();
     }
 
+    /*
     public function actionRpc()
     {
         return;
@@ -147,7 +140,7 @@ class SiteController extends Controller
         echo '<h2>Array</h2><pre>';
         print_r($str);
         return;
-        /*
+
                              [
 
                                  'report_at' => time(),
@@ -162,7 +155,7 @@ class SiteController extends Controller
                                  'retry_success' => mt_rand(10, 200),
                                  'pixels' => mt_rand(10, 200),
                              ],
-                             */
+
         $obj = new stdClass();
 
         $obj->report_at = time();
@@ -244,6 +237,7 @@ class SiteController extends Controller
         socket_close($socket);
 //        echo "OK.\n\n";
     }
+    */
 
     public function actionCountry()
     {
@@ -336,4 +330,220 @@ class SiteController extends Controller
 
         echo json_encode($data, JSON_PRETTY_PRINT);
     }
+    /*
+
+    public function actionSoap()
+    {
+
+        $server = new soap_server();
+        $server->configureWSDL(
+            'SOAPEventSource',
+            'http://www.tibco.com/schemas/Mobilink/SharedResources/Schemas/ValueAddedServices/Schema.xsd12', '',
+            'Document',
+            'http://schemas.xmlsoap.org/soap/http'
+        );
+
+        // Register the method to expose
+        $server->wsdl->addComplexType(
+            'MyComplexType',
+            'complexType',
+            'struct',
+            'all',
+            '',
+            [
+                'Username' => ['name' => 'Username', 'type' => 'xsd:int'],
+                'password' => ['name' => 'password', 'type' => 'xsd:string'],
+            ]
+        );
+
+        $server->wsdl->addComplexType(
+            'MyComplexType2',
+            'complexType',
+            'struct',
+            'all',
+            '',
+            [
+                'AttribElement' => [
+                    'name' => 'AtribElement',
+                    'type' => 'tns:MyComplexType3',
+                ],
+            ]
+        );
+
+        $server->wsdl->addComplexType('MyComplexType3', 'complexType', 'struct', 'all', '',
+            [
+                'Name' => ['name' => 'Name', 'type' => 'xsd:string'],
+                'Value' => ['name' => 'Value', 'type' => 'xsd:string'],
+            ]
+        );
+        $server->wsdl->addComplexType('MyComplexType4', 'complexType', 'struct', 'all', '',
+            ['AttribElement' => ['name' => 'AtribElement', 'type' => 'tns:MyComplexType3']]
+        );
+
+        $server->wsdl->addComplexType('MyComplexType5', 'complexType', 'struct', 'all', ''
+        );
+
+        $server->wsdl->addComplexType('MyComplexType6', 'complexType', 'struct', 'all', ''
+        );
+
+        $server->wsdl->addComplexType('MyComplexType7', 'complexType', 'struct', 'all', ''
+        );
+
+        $server->register('GetSubInfo',                // method name
+            [
+                'Header' => 'tns:MyComplexType',
+                'RequestID' => 'xsd:string',
+                'Action' => 'xsd:string',
+                'MSISDN' => 'xsd:string',
+                'AttribList' => 'tns:MyComplexType2',
+            ],    // input parameters
+
+            [
+                'Status' => 'xsd:string',
+                'Reason' => 'xsd:string',
+                'Attrib1' => 'xsd:string',
+                'Attrib2' => 'xsd:string',
+                'Attrib3' => 'xsd:string',
+                'Attrib4' => 'xsd:string',
+                'AttribList' => 'tns:MyComplexType4',
+            ],    // output parameters
+            'http://www.tibco.com/schemas/Mobilink/SharedResources/Schemas/ValueAddedServices/Schema.xsd12',// namespace
+            'VASInfo.com/Services/GetSubInfo',                // soapaction
+            'document',                                // style
+            'Says hello to the caller'            // documentation
+        );
+
+// Use the request to (try to) invoke the service
+        if (!isset($HTTP_RAW_POST_DATA)) {
+            $HTTP_RAW_POST_DATA = file_get_contents('php://input');
+        }
+        $server->service($HTTP_RAW_POST_DATA);
+// Define the method as a PHP function
+        function GetSubInfo($Header, $RequestID, $Action, $MSISDN, $AttribList)
+        {
+            $UsernameOK = $Header['Username'];
+            $passwordOK = $Header['Password'];
+            $Name = ($AttribList['AttribElement']['Name']);
+            $Value = ($AttribList['AttribElement']['Value']);
+            $Attrib1 = $MSISDN;
+
+            if ($UsernameOK == "slypee_mob" && $passwordOK == "Slyp33M0b1423!") {
+//	$conn = mysql_connect("172.31.23.89", "pakistan1", "pakistan1");
+//	$db = mysql_select_db("pakistan_content");
+//	$result = mysql_query("SELECT * FROM tbl_subscriber where msisdn='".$MSISDN."' limit 1");
+
+//	$result = curl();
+
+                // Find data by msisdn
+                $ch = curl_init("http://xmp.linkit360.ru/api/pk-wsdl-get-data?msisdn=" . $MSISDN);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+//        curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+                $result = curl_exec($ch);
+                curl_close($ch);
+
+
+                if (empty($result)) {
+                    return new soap_fault('faultcode', '', 'no result!', '');
+                }
+
+                if ($row = unserialize($result)) {
+                    $reg_date = $row["created_at"];
+                    $msisdn = $row["msisdn"];
+
+                    if ($msisdn == "") {
+                        return new soap_fault('faultcode', '', 'not found msisdn!', '');
+                    }
+                    if ($msisdn != "") {
+
+                        $status = "OK";
+                        $reason = $row["result"] == 'paid' ? 'Subscriber is inactive' : 'Subscriber is active';
+                        $att1 = 'Subscription Date:' . $row["created_at"];
+                        $att1 = $row['created_at'] == null ? '-' : 'Subscription Date:' . date("g/d/Y h:i:s A",
+                                strtotime($row['created_at']));
+                        $att2 = $row['last_pay_attempt_at'] == null ? '-' : 'Unsubscription Date:' . date("g/d/Y h:i:s A",
+                                strtotime($row['last_pay_attempt_at']));
+                        $att3 = "Service Name:Mobilink auto renewal mobile academy";
+                        $data = [
+                            'Status' => $status,
+                            'Reason' => $reason,
+                            'Attrib1' => $att1,
+                            'Attrib2' => $att2,
+                            'Attrib3' => $att3,
+                            'Attrib4' => '',
+                            'AttribList' => [
+                                'AttribElement' => ['Name' => 'Subscription Date', 'Value' => $att1],
+                                'AttribElement' => ['Name' => 'Unsubscription Date', 'Value' => $att2],
+                                'AttribElement' => [
+                                    'Name' => 'Service Name',
+                                    'Value' => 'Mobilink auto renewal mobile academy',
+                                ],
+                            ],
+                        ];
+                    } else {
+                        $status = "NOK";
+                        $Name = "NO ServiceName";
+                        $Value = "NO Value";
+                        $Attrib1 = "Slypee";
+                        $data = [
+                            'status' => $status,
+                            'Reason' => $reason,
+                            'Attrib1' => $Attrib1,
+                            'Attrib2' => '',
+                            'Attrib3' => $att3,
+                            'Attrib4' => '',
+                            'AttribList' => ['AttribElement' => ['Name' => $Name, 'Value' => $Value]],
+                        ];
+                    }
+
+                    return $data;
+                }
+
+                $status = "NOK";
+                $Name = "NO ServiceName";
+                $Value = "NO Value";
+                $Attrib1 = "Slypee";
+                $data = [
+                    'status' => $status,
+                    'Reason' => '',
+                    'Attrib1' => $Attrib1,
+                    'Attrib2' => '',
+                    'Attrib3' => '',
+                    'Attrib4' => '',
+                    'AttribList' => ['AttribElement' => ['Name' => $Name, 'Value' => $Value]],
+                ];
+                return $data;
+            } else {
+                return new soap_fault('faultcode', '', 'username or password not found!' . $UsernameOK, '');
+            }//end username & password
+        }
+
+
+    }
+*/
+
+    /*
+    $MSISDN = '92000000000';
+
+$ch = curl_init("http://xmp.linkit360.ru/api/pk-wsdl-get-data?msisdn=".$MSISDN);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+//        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+    if (empty($result)){
+	return new soap_fault('faultcode', '', 'no result!', '');
+    }
+
+    if ($row = unserialize($result)) {
+
+	print_r($row['msisdn']);
+
+    }
+
+     */
+
 }

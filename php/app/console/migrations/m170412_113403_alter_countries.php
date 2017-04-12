9d059b9cb54b89cb8e9d05472f952e4cf9ad987c
Caravus <caravus@caravus.org>
2017-04-12 16:15:38 +0300

    Countries flags and currency

--- php/app/console/migrations/m170412_113403_alter_countries.php
+++ php/app/console/migrations/m170412_113403_alter_countries.php
@@ -8,6 +8,7 @@ class m170412_113403_alter_countries extends Migration
     {
         $this->execute("ALTER TABLE xmp_countries ADD flag VARCHAR(64) DEFAULT 'Unknown' NOT NULL;");
         $this->execute("ALTER TABLE xmp_countries ADD currency VARCHAR(32) DEFAULT '' NOT NULL;");
+        $this->execute("SELECT setval('xmp_countries_id_seq', (SELECT max(id) FROM xmp_countries));");
     }
 
     public function safeDown()

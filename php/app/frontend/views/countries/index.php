3fa45de3564c653d3a5a94d137b1473774730030
Caravus <caravus@caravus.org>
2017-04-19 17:27:37 +0300

    countries

--- php/app/frontend/views/countries/index.php
+++ php/app/frontend/views/countries/index.php
@@ -10,28 +10,37 @@ use yii\grid\GridView;
 
 $this->title = 'Countries';
 $this->params['breadcrumbs'][] = $this->title;
+
+$bundle = \common\assets\InspiniaAsset::register($this);
+
 ?>
-<div class="col-lg-6">
+<div class="col-lg-4">
     <div class="ibox">
         <div class="ibox-content">
+            <p>
+                <?php
+                echo Html::a(
+                    'Create Country',
+                    ['create'],
+                    ['class' => 'btn btn-success']
+                );
+                ?>
+            </p>
             <?php
-            echo Html::a(
-                'Create Country',
-                ['create'],
-                ['class' => 'btn btn-success']
-            );
-
             echo GridView::widget(
                 [
                     'dataProvider' => $dataProvider,
                     'columns' => [
-                        'id',
-                        'name',
-                        'code',
-//                        'status',
-                        'iso',
-                        'priority',
+                        [
+                            'attribute' => 'name',
+                            'filter' => false,
+                            'content' => function ($row) use ($bundle) {
+                                return '<img src="' . $bundle->baseUrl . '/img/flags/32/' . $row['flag'] . '.png">&nbsp;' . $row['name'];
+                            },
+                        ],
 
+                        'code',
+                        'iso',
                         [
                             'class' => 'yii\grid\ActionColumn',
                             'headerOptions' => [
